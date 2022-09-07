63e48c128149bb163a655ef70dc294e6b7245334
Amal v s <42992006+alchemist123@users.noreply.github.com>
2022-09-08 01:39:48 +0530

    Update employeeRouter.js

--- routers/employeeRouter.js
+++ routers/employeeRouter.js
@@ -2,6 +2,6 @@ const express = require("express");
 const empRouter = express.Router();
 const empController = require("../controllers/employee.controller");
 const { employeeVerifyAccessToken } = require("../middlewares/auth.middleware");
-empRouter.post("/getEmployee", adminVerifyAccessToken ,empController.getEmployee);
+empRouter.post("/getEmployee", employeeVerifyAccessToken ,empController.getEmployee);
 
 module.exports = empRouter;
