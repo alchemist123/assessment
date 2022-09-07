867f8088818f990d20bc82048a445a89d59791f5
Amal v s <42992006+alchemist123@users.noreply.github.com>
2022-09-08 01:39:26 +0530

    Update admin.router.js

--- routers/admin.router.js
+++ routers/admin.router.js
@@ -5,5 +5,5 @@ const { adminVerifyAccessToken } = require("../middlewares/auth.middleware");
 adminRouter.post("/addEmployee", adminVerifyAccessToken ,AdminController.addEmployees);
 adminRouter.post("/getEmployee", adminVerifyAccessToken ,AdminController.getEmployee);
 adminRouter.post("/deleteRecord", adminVerifyAccessToken ,AdminController.deleteRecord);
-
+adminRouter.post("/updateRecord", adminVerifyAccessToken ,AdminController.updateRecord);
 module.exports = adminRouter;
